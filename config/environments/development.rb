06b7a28a976f274d891eb195878ae101ffdeb08c
EC2 Default User <135218738+YasuyoshiSakai@users.noreply.github.com>
2023-08-13 11:46:16 +0000

    yu-za-

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.hosts << "0191910108904d28abdf3db3428f6429.vfs.cloud9.ap-southeast-2.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,56 @@
 Rails.application.routes.draw do
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+  namespace :admin do
+
+    root to: 'homes#top'
+
+    resources :genres, only: [:index, :create, :edit, :update]
+    # ジャンル（管理画面、データ登録、編集、データ更新）
+
+    resources :customers, only: [:index, :show, :edit, :update]
+    # 顧客（一覧、詳細、編集、情報の更新）
+
+    get 'admin/search' => 'searchs#search', as: 'search'
+
+  end
+
+
+
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  root to: 'public/homes#top'
+  scope module: :public do
+
+    get "/homes/about" => "homes#about", as: "about"
+
+    resources :words, only: [:index, :show]
+    # 話題（一覧、詳細）
+
+    get 'customers/mypage' => 'customers#show'
+    # 顧客のマイページ
+
+    get 'customers//information/edit' => 'customers#edit'
+    # 顧客の会員登録情報編集
+
+    patch 'customers/information/update' => 'customers#update'
+    # 顧客の登録情報更新
+
+    get 'customers/check' => 'customers#check', as: 'customers_check'
+    # 顧客の退会確認画面
+
+    get 'customers/withdrawal' => 'customers#withdrawal', as: 'customers_withdrawal'
+    # 顧客の退会処理(ステータスの更新)
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
