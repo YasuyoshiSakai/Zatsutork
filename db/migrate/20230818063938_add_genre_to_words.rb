c1b0126335cf5430a43637cce4800196eca1989a
EC2 Default User <135218738+YasuyoshiSakai@users.noreply.github.com>
2023-08-28 10:19:17 +0000

    genres

--- db/migrate/20230818063938_add_genre_to_words.rb
+++ db/migrate/20230818063938_add_genre_to_words.rb
@@ -2,6 +2,6 @@ class AddGenreToWords < ActiveRecord::Migration[6.1]
   def change
     add_column :words, :genre_id, :integer, null: false
     add_index :words, :genre_id
-    add_foreign_key :words, :genres
+    #add_foreign_key :words, :genres
   end
 end
